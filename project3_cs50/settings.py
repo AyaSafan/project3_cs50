2d6302cf047bb7144a4511b2673821137df35e30
AyaSafan <ayasafan16@gmail.com>
2020-05-12 22:34:05 +0200

    whitenoise documentation

--- project3_cs50/settings.py
+++ project3_cs50/settings.py
@@ -59,6 +59,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 ROOT_URLCONF = 'project3_cs50.urls'
 
 TEMPLATES = [
